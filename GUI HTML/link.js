3c9829dcfa8d7421eb6cc4ee421be7367036e177
thangdangsad <21011631@st.phenikaa-uni.edu.vn>
2023-05-20 08:11:24 +0700

    update

--- GUI HTML/link.js
+++ GUI HTML/link.js
@@ -1,6 +1,7 @@
 let popupAdd = document.getElementById('addcustomer');
 function openPopup() {
-popupAdd.classList.add('open-popup');     
+popupAdd.classList.add('open-popup');
+popuplist.classList.remove('open-popup');     
 }
 function closePopup() {
 popupAdd.classList.remove('open-popup');         
@@ -11,7 +12,8 @@ popupAdd.classList.remove('open-popup');
 let popuplist = document.getElementById('listcustomer');
 
 function openListcustomer() {
-    popuplist.classList.add('open-popup');     
+    popuplist.classList.add('open-popup');   
+    popupAdd.classList.remove('open-popup');  
   }
   function closeListcustomer() {
     popuplist.classList.remove('open-popup');         
